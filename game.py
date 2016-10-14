dd18f6f5bdd0dc919aeec7f7f866e07b55ce5606
Nikhil Saldanha <nikhil.saldanha691@gmail.com>
2016-10-14 15:54:38 +0530

    Documentation

--- game.py
+++ game.py
@@ -5,42 +5,48 @@ import ai,copy,math
 import argparse
 class Game(object):
 
+	# create a instance of Board and initialize the board to all zeroes
+	board = Board()
 
-
-	def __init__(self,board=[[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]],ai=False, step=False):
-		self.board = Board(board)
-		self.score = 0
+	#
+	def __init__(self,b=None, score=0, win=2048, ai=False, step=False):
+		if b:
+			self.board.setBoardParams(b)
+		self.board.setWinGoal(win)
+		self.score = self.board.setScore(score)
 		self.ai = ai
 		self.step = step
-		
-		
+
+
 	def readMove(self):
 		return keypress.getKey()
-	
+
 	def clrscr(self):
-		if os.name == 'nt':	
+		if os.name == 'nt':
 			os.system('cls')
-				
+
 		else:
-			os.system('clear')	
-		
-		
+			os.system('clear')
+
+
 	def game(self):
 
 		if not self.ai:
-			while not self.board.won() or self.board.canMove():
-			
+			print self.board.board
+
+			while not self.board.won() and self.board.canMove():
+
 				# self.board.drawBoard(self.board.getCells())
-				
+
 				key = self.readMove()
-				self.clrscr()
-				
+
 				if key:
 					if self.board.move(key):
 						self.board.tileGen(1)
-					self.board.drawBoard()
+						self.clrscr()
+						self.board.drawBoard()
 				else:
-					break
+					exit(0)
 		else:
 			print 'Press any key to begin...'
 			while True:
@@ -72,12 +78,12 @@ class Game(object):
 					# d = self.expectimax(2)
 				# else:
 					# d = self.minimax(3)
-					
+
 				if self.board.emptyTiles()<=7:
 					d = self.minimax(7)
 				else:
-					d=self.expectimax(2)	
-				
+					d=self.expectimax(2)
+
 				if d:
 					self.board.move(d)
 					self.board.tileGen(1)
@@ -87,9 +93,9 @@ class Game(object):
 
 				# raw_input(' ')
 			return self.board.score()
-			
-			
-			
+
+
+
 	def minimax(self, depth):
 		vals = {}
 		moves = ['up','left','right','down']
@@ -100,30 +106,30 @@ class Game(object):
 			b = copy.deepcopy(self.board)
 			if b.move(m):
 				vals[m] = self.value(b, depth, alpha, beta)
-		
+
 		if vals:
 			return max(vals, key=vals.get)
 		else:
 			return None
-			
-			
-			
+
+
+
 	def value(self, b, depth,alpha, beta):
 		if depth == 1:
 			return self.evalfn(b)
-		
+
 		# b = copy.deepcopy(b)
 		if depth%2==0:
 			return self.minval(b,depth-1, alpha, beta)
 		else:
 			return self.maxval(b,depth-1, alpha, beta)
-	
-	
-	
+
+
+
 	def maxval(self,state,depth,a,b):
 		v = float('-inf')
 		moves = ['up','left','right','down']
-		
+
 		for m in moves:
 			s1 = copy.deepcopy(state)
 			i = 0
@@ -140,59 +146,59 @@ class Game(object):
 						# break
 			if s1.move(m):
 				v = max(v,self.emvalue(s1, depth))
-				
+
 			if v >= b:
 				return v
 			a = max(a,v)
 		return v
-		
+
 	def minval(self, state, depth, a, b):
-		v = float('inf')		
+		v = float('inf')
 		empty = state.emptyTiles()
 		for pos in empty:
 			s1 = copy.deepcopy(state)
 			s1.tileGen(1,pos)
 			v = min(v,self.value(s1, depth,a,b))
-			
+
 			if v <= a:
 				return v
 			b = min(b,v)
-			
+
 		return v
-		
+
 	def expectimax(self, depth):
 		vals = {}
 		moves = ['up','left','right','down']
-		depth = 2*depth	
-		
+		depth = 2*depth
+
 		for m in moves:
 			b = copy.deepcopy(self.board)
 			if b.move(m):
 				vals[m] = self.emvalue(b, depth)
-		
+
 		if vals:
 			return max(vals, key=vals.get)
 		else:
 			return None
-			
-			
-			
+
+
+
 	def emvalue(self, b, depth):
 		if depth == 1:
 			return self.evalfn(b)
-		
+
 		# b = copy.deepcopy(b)
 		if depth%2==0:
 			return self.expval(b,depth-1)
 		else:
 			return self.emaxval(b,depth-1)
-	
-	
-	
+
+
+
 	def emaxval(self,state,depth):
 		v = float('-inf')
 		moves = ['up','left','right','down']
-		
+
 		for m in moves:
 			s1 = copy.deepcopy(state)
 			i = 0
@@ -210,7 +216,7 @@ class Game(object):
 			if s1.move(m):
 				v = max(v,self.emvalue(s1, depth))
 		return v
-		
+
 	def expval(self, state, depth):
 		v = 0
 		empty = state.emptyTiles()
@@ -218,12 +224,12 @@ class Game(object):
 			s1 = copy.deepcopy(state)
 			s1.tileGen(1,pos)
 			v += (self.emvalue(s1, depth))*(1.0/len(empty))
-			
+
 		return v
-			
-		
-	
-		
+
+
+
+
 	def evalfn(self, b2, p=False):
 		mono = b2.monotonicity3()
 		smooth = b2.smoothness8()
@@ -249,12 +255,12 @@ class Game(object):
 		else:
 			blocked = -200
 		# if p:
-			# print 'blockd1', blocked 
-		
+			# print 'blockd1', blocked
+
 		# else:
 			# bonus += 100
 		# blocked += bonus
-		
+
 		# if p:
 			# print 'blocked2', blocked
 		# temp = 2.7*b2.getEmptyCells() + mono + math.log(b2.highestTile()[0]) + 0.1*smooth# - 2.7*ai.manhatDist(b2.highestTile())
@@ -266,13 +272,13 @@ class Game(object):
 		deathPenalty = 0
 		if b2.filled() and not b2.canMove():
 			deathPenalty = -10000000
-		
+
 		corner = ai.atCorner(high)
 		eW = 0
 		if (b2.board[0][2] != 0 and b2.board[0][3] != 0 and high > 128):
 			two = math.log(b2.board[0][2],2)
 			three = math.log(b2.board[0][3],2)
-			
+
 			if two-three >= 5:
 				eW = -3
 		bw = 80.
@@ -288,7 +294,7 @@ class Game(object):
 			# temp = 3*blocked + 4*ai.atCorner(high) + 18*empty + 2.3*mono + 1*(n-o) + 1.*smooth
 			temp = bw*blocked + acw* + ew*empty + mw*mono + scw*(n-o) + sw*smooth+deathPenalty
 			# temp = mono + deathPenalty + b2.sm()
-			
+
 			# temp = 4*blocked + 5.7*ai.atCorner(high) + 17*empty + 10.3*mono  + 1.11*smooth + deathPenalty#+ 1.12**(n-o)
 		else:
 			##### most recent
@@ -303,14 +309,14 @@ class Game(object):
 			# print 'mono', mw*mono
 			# print 'score', scw*(n-o)
 			# print 'smooth', sw*smooth
-			
-		return temp
-			
 
-			
+		return temp
+
+
+
 if __name__ == '__main__':
 
-	parser = argparse.ArgumentParser(description='Your favourite 2048 now in the terminal! Play it or have an AI solve it for you')
+	parser = argparse.ArgumentParser(description='Your favourite 2048, now in the terminal! Play it or have an AI solve it for you')
 	parser.add_argument('--ai', action='store_true', help='AI Flag')
 	parser.add_argument('--step', action='store_true', help='Run the AI Step by Step')
 	args = parser.parse_args()
--- keypress.py
+++ keypress.py
@@ -1,28 +1,37 @@
 import os
-def getKey():
 
+# getKey():
+# 	Waits for a keyboard input and determines which key was pressed, up, down, left or right.
+#	It supports windows, linux and OSX
+#	returns:
+#		'up', 'down', 'left' or 'right' respectively if the up, down, left or right keys are pressed.
+#		None is returned, otherwise.
+
+def getKey():
+	# if windows
 	if os.name == 'nt':
 
 		from msvcrt import getch
 		UP,DOWN,LEFT,RIGHT = 80,72,75,77
 		key = ord(getch())
-		
+
 		if key == 224:
 			key = ord(getch())
-			
+
 			if key == 80:
 				return 'down'
-				
+
 			elif key == 72:
 				return 'up'
-				
+
 			elif key == 75:
 				return 'left'
-				
+
 			elif key == 77:
 				return 'right'
-				
+
 		return None
+	# not windows
 	else:
 		import sys, tty, termios
 		fd = sys.stdin.fileno()
@@ -32,6 +41,7 @@ def getKey():
 		c = sys.stdin.read(3)
 		termios.tcsetattr(fd, termios.TCSADRAIN, old)
 
+		print c
 		if c=='\x1b[A':
 			return 'up'
 		elif c=='\x1b[B':
